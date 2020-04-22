33df84a5eb58ff55f67afe917fb99ba94c3cff73
Nathan Ma <ndma@ucsc.edu>
2020-04-21 23:38:41 -0700

    Setup

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,32 +4,13 @@ class Load extends Phaser.Scene {
     }
 
     preload(){       
-        // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion38', './assets/explosion38.wav');
-        this.load.audio('sfx_explosion1', './assets/explosion1.wav');
-        this.load.audio('sfx_explosion2', './assets/explosion2.wav');
-        this.load.audio('sfx_explosion3', './assets/explosion3.wav');
-        this.load.audio('sfx_explosion4', './assets/explosion4.wav');
-        this.load.audio('sfx_fship_explosion', './assets/fship_explosion.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
-        this.load.audio('8bit_music', './assets/8bit chillout.mp3');
-
-        // load sprites
-        this.load.image('rocket', './assets/rocket.png');
-        this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('fastship', './assets/fastship.png');
-        this.load.image('starfield', './assets/starry_night.jpg');
-
-        // load animations
-        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
-        this.load.spritesheet('fship_explosion', './assets/fship_explosion.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 9});
-
+        // loading bar frame
         var progressBar = this.add.graphics();
         var progressBox = this.add.graphics();
         progressBox.fillStyle(0x222222, 0.8);
         progressBox.fillRect(game.config.width/4, game.config.height/2, 320, 50);
-
+        
+        // active loading bar
         var width = this.cameras.main.width;
         var height = this.cameras.main.height;
         var loadingText = this.make.text({
@@ -41,6 +22,8 @@ class Load extends Phaser.Scene {
                 fill: '#ffffff'
             }
         });
+
+        // loading percent
         loadingText.setOrigin(0.5, 0.5);
         var percentText = this.make.text({
             x: width / 2,
@@ -53,6 +36,7 @@ class Load extends Phaser.Scene {
         });
         percentText.setOrigin(0.5, 0.5);
 
+        // loader
         this.load.on('progress', function (value) {
             // console.log(value);
             percentText.setText(parseInt(value * 100) + '%');
@@ -61,10 +45,12 @@ class Load extends Phaser.Scene {
             progressBar.fillRect(game.config.width/4 + 10, game.config.height/2 + 10, 300 * value, 30);
         });
 
+        // load tracking
         this.load.on('fileprogress', function (file) {
             console.log(file.src);
         });
-         
+        
+        // destroy loading bar
         this.load.on('complete', function () {
             console.log('complete');
             progressBar.destroy();
