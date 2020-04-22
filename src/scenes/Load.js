17a5f3234135b090e139c72173dccde597ab7b00
Nathan Ma <ndma@ucsc.edu>
2020-04-21 23:28:34 -0700

    Setup

---
+++ src/scenes/Load.js
@@ -0,0 +1,68 @@
+class Load extends Phaser.Scene {
+    constructor() {
+        super("loadScene");
+    }
+
+    preload(){
+        // outer gray rectangle
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(game.config.width/4, game.config.height/2, 320, 50);
+
+        var width = this.cameras.main.width;
+        var height = this.cameras.main.height;
+        var loadingText = this.make.text({
+            x: width / 2 + 10,
+            y: height / 2 - 30,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        // percent complete
+        var percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 25,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        this.load.on('progress', function (value) {
+            // console.log(value);
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(game.config.width/4 + 10, game.config.height/2 + 10, 300 * value, 30);
+        });
+
+        // loading files in
+        this.load.on('fileprogress', function (file) {
+            console.log(file.src);
+        });
+         
+        // destroy loading bar
+        this.load.on('complete', function () {
+            console.log('complete');
+            progressBar.destroy();
+            progressBox.destroy();	
+            loadingText.destroy();
+            percentText.destroy();
+        });
+    }
+
+    create() {
+        this.scene.start("menuScene");
+    }
+
+    update(){        
+        
+    }
+}
