e9583e5f8ebd4ac7495c6692f8b1a9d4e387ac7b
Nathan Ma <ndma@ucsc.edu>
2020-04-28 22:31:03 -0700

    Organize

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,34 +4,38 @@ class Load extends Phaser.Scene {
     }
 
     preload(){
-        this.load.path = "./assets/";
-
-        this.load.image('start', 'start.png');
+        this.load.path = "./assets/bg";
         this.load.image('fox1_bg', 'dark_forest.png');
-        this.load.image('obstacle', 'obstacle.png');
         this.load.image('tile_block', 'forest_tile.png');
-        this.load.image('dream_border', 'dream_border.png')
 
         // these need to be atlas later
+        this.load.path = "./assets/fox_sprites/";
         this.load.spritesheet('fox1', 'base_fox.png', {frameWidth: 128, frameHeight: 87, startFrame: 0, endFrame: 3});
         this.load.spritesheet('fox2', 'base_fox2.png', {frameWidth: 148, frameHeight: 96, startFrame: 0, endFrame: 4});
         this.load.spritesheet('fox3', 'water_fox1.png', {frameWidth: 148, frameHeight: 96, startFrame: 0, endFrame: 4});
         this.load.spritesheet('death', 'fship_explosion.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 9}); // dummy
 
+        this.load.path = "./assets/ost/";
+        this.load.audio('menu_ost', 'I_Am_Different.mp3');
+        this.load.audio('fox1_ost', 'Dreams_of_the_Brilliant.mp3');
+        this.load.audio('fox2_ost', 'The_Long_Journey.mp3');
+        this.load.audio('fox3_ost', 'Water_Spirit.mp3');
+        this.load.audio('fox4_ost', 'Wind_Spirit.mp3');
+        this.load.audio('fox5_ost', 'Earth_Spirit.mp3');
+        this.load.audio('fox6_ost', '.mp3');
+        this.load.audio('fox7_ost', 'Days_of_Summer.mp3');
+        this.load.audio('fox8_ost', 'Days_of_Winter.mp3');
+        this.load.audio('fox9_ost', 'Into_The_Sky.mp3');
+        this.load.audio('death_ost','Autumn_Rain.mp3')
+        
+        this.load.path = "./assets/misc/";
+        this.load.image('start', 'start.png');
+        this.load.image('obstacle', 'obstacle.png');
+        this.load.image('dream_border', 'dream_border.png')
+
+        this.load.path = "./assets/sfx";
         this.load.audio('jump_sfx', 'sfx/jump_sfx.wav');
 
-        this.load.audio('menu_ost', 'ost/I_Am_Different.mp3');
-        this.load.audio('fox1_ost', 'ost/Dreams_of_the_Brilliant.mp3');
-        this.load.audio('fox2_ost', 'ost/The_Long_Journey.mp3');
-        this.load.audio('fox3_ost', 'ost/Water_Spirit.mp3');
-        this.load.audio('fox4_ost', 'ost/Wind_Spirit.mp3');
-        this.load.audio('fox5_ost', 'ost/Earth_Spirit.mp3');
-        this.load.audio('fox6_ost', 'ost/.mp3');
-        this.load.audio('fox7_ost', 'ost/Days_of_Summer.mp3');
-        this.load.audio('fox8_ost', 'ost/Days_of_Winter.mp3');
-        this.load.audio('fox9_ost', 'ost/Into_The_Sky.mp3');
-        this.load.audio('death_ost','ost/Autumn_Rain.mp3')
-        
 
         /* test loading bar buffer*/
         // this.load.image('background', 'images/tut/background.png');
